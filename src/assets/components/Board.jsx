7ee04a70509483a28fd40b3624fe82c76df34515
amuuo-dev <anthonmuuo92@gmail.com>
2024-04-05 12:11:08 +0300

    updated the nextSquare for efficient takin turns

--- src/assets/components/Board.jsx
+++ src/assets/components/Board.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import Square from "./Square";
 const Board = () => {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquare = squares.slice();
-    nextSquare[i] = "X";
+    if (xIsNext) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "O";
+    }
     setSquares(nextSquare);
+    setXIsNext(!xIsNext);
   }
   return (
     <>
